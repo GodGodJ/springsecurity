a347cf4283d8329dfa7fd68320a8c5c290dea71b
GodGodJ <2635121825@qq.com>
2021-06-07 10:15:45 +0800

    第二次提交

---
+++ src/main/java/com/example/springsecurity/model/LoginUser.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.model;
+
+import lombok.Data;
+
+@Data
+public class LoginUser {
+
+    private String username;
+    private String password;
+    private Integer rememberMe;
+
+
+}
